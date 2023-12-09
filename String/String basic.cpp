77ec5ef304e8a92453a692b2bf0c26ceb6baab22
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-09 11:45:36 +0530

    Create String basic.cpp

---
+++ String/String basic.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+
+{
+    // char arr[20];
+    // cout<<"Enter the array character ";
+    // cin>>arr;
+    // cout<<arr;
+    
+    // string s="Naseem Choudhary";
+    // cout<<s;
+    
+    // string s;
+    // cout<<"ENter the string ";
+    // cin>>s;
+    // cout<<s;  // herre we can't print naseem choudhary onyl print naseem
+    
+    // string s;
+    // cout<<"enter the string ";
+    // getline(cin,s);   // here print naseem choudhary
+    // cout<<s<<endl;
+    
+    //  print the size ya length of string 
+    // cout<<"size of string ";
+    // cout<<s.size();
+    
+    
+    
+    // i want ot print like it How are "you" naseem
+    
+    string s="how are \"you\"naseem";
+    cout<<s;
+
+  https://www.youtube.com/watch?v=FkaIZAQKmWU&list=PLQEaRBV9gAFu4ovJ41PywklqI7IyXwr01&index=36
+    
+
+    
+    
+    
+    
+    
+}
