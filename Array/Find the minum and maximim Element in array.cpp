b9cc595d2245d8c4d1dce435e055b711d780fb2c
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-13 11:19:37 +0530

    Update Find the minum and maximim Element in array.cpp

--- Array/Find the minum and maximim Element in array.cpp
+++ Array/Find the minum and maximim Element in array.cpp
@@ -21,4 +21,39 @@ int main()
     }
     cout<<ans;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////
+maxi and min take a input 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Enter the array size: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Initialize max and min with the first element of the array
+    int max = arr[0];
+    int min = arr[0];
+
+    // Find the maximum and minimum values
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << "Maximum value: " << max << endl;
+    cout << "Minimum value: " << min << endl;
+
+    return 0;
+}
