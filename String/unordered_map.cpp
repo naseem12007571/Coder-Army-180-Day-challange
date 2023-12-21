76ff883cd698101f5f701fcaad30075d799ff01d
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-21 13:29:06 +0530

    Create unordered_map.cpp

---
+++ String/unordered_map.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      unordered_map<int,string>map;
+      map[1]="naseem";
+      map[2]="choudhary";
+      map[3]="moradabad";
+      
+      for(auto x : map)
+      cout<< x.first<<" "<<x.second<<endl;
+    
+}
