322088fd355e592043f2804d7e438e64f856157a
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-30 16:01:18 +0530

    Update String basic.cpp

--- String/String basic.cpp
+++ String/String basic.cpp
@@ -34,11 +34,50 @@ int main()
     cout<<s;
 
   https://www.youtube.com/watch?v=FkaIZAQKmWU&list=PLQEaRBV9gAFu4ovJ41PywklqI7IyXwr01&index=36
+      
     
+}
 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    // char arr[100];
+    // cin>>arr;
+    // cout<<arr;
+    
+    // string s="naseem";
+    // cout<<s;
+    
+    // string s;
+    // cin>>s;
+    // cout<<"string is "<<s;
     
     
+    // //////  take the input from user
+    // string s;
+    // cout<<"enter the string character ";
+    // getline(cin,s);
+    // cout<<"your string is "<<s<<endl;
+    
+    // // check the size of string 
+    // cout<<" size of string "<<s.size();
     
     
+    ///////   apend 
+    // string s1="naseem ",s2="moradabad";
+    // // cout<<s1+s2;
+    // // or append 
+    // s1.append(s2);
+    // cout<<s1;
+    
+    // push back of use 
+    string s="Naseem";
+    // s.push_back('c');
+    s.pop_back();
+    cout<<s;    
     
 }
